9e8c54c2b3792ef77c3731218090e081e080c710
Pedro da Costa Lopes <pedrolopes.programador013@gmail.com>
2026-03-17 19:58:47 -0300

    feat: desenvolvendo os exercicios propostos!

--- Exercicios-Propostos/src/Main.java
+++ Exercicios-Propostos/src/Main.java
@@ -1,8 +1,68 @@
 import java.util.Scanner;
-public class Main {
-    public static void main() {
-        Scanner input = new Scanner(System.in);
 
+public class Main {
+    public static Scanner input = new Scanner(System.in);
+
+    public static void main() {
+
+        System.out.println("Escolha o exercicio: " +
+                "\n01) Produtos e Descontos;" +
+                "\n02) Fibonacci;" +
+                "\n03) Horário;" +
+                "\n04) Datas;" +
+                "\n05) Salários;" +
+                "\n06) Valores Primos;" +
+                "\n07) Repetições;" +
+                "\n08) Merge;" +
+                "\n09) Sair;");
+
+        int escolhaUser = input.nextInt();
+
+        switch (escolhaUser) {
+            case 1:
+                ProdutosDescontos();
+                main();
+                break;
+            case 2:
+                Fibonacci();
+                main();
+                break;
+            case 3:
+                Horario();
+                main();
+                break;
+            case 4:
+                //Datas();
+                main();
+                break;
+            case 5:
+                //Salario();
+                main();
+                break;
+            case 6:
+                //ValoresPrimos();
+                main();
+                break;
+            case 7:
+                //Repeticoes();
+                main();
+                break;
+            case 8:
+                //Merge();
+                main();
+                break;
+            case 9:
+                System.out.println("Saindo...");
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Valor inválido! Digite novamente!");
+                main();
+                break;
+        }
+    }
+
+    public static void ProdutosDescontos() {
         System.out.println("=== Desconto Sobre Produtos ===");
 
         System.out.println("Digite o nome do produto");
@@ -46,6 +106,9 @@ public class Main {
             System.out.println("Valor final: R$" + precoFinal);
         }
 
+    }
+
+    public static void Fibonacci() {
         System.out.println("=== Verificador Fibonacci ===");
 
         System.out.println("Digite um número");
@@ -68,5 +131,27 @@ public class Main {
         } else {
             System.out.println("O número " + numUser + ", não está contido na sequência de Fibonacci!");
         }
+    }
+
+    public static void Horario() {
+        System.out.println("=== Conversor de Horarios ===");
+
+        System.out.println("Digite um número de segundos:");
+        int segundosUser = input.nextInt();
+
+        int minutos = 0;
+        int horas = 0;
+
+        if (segundosUser >= 60) {
+            minutos = segundosUser / 60;
+            segundosUser = segundosUser % 60;
+            if (minutos >= 60) {
+                horas = minutos / 60;
+                minutos = minutos % 60;
+            }
+        }
+
+        System.out.println(horas + " Horas " + minutos + " Minutos " + segundosUser + " Segundos");
+
     }
 }
