680f754b864b5d9e18e03b28e00db579bcf7b36d
Pedro da Costa Lopes <pedrolopes.programador013@gmail.com>
2026-04-27 21:47:10 -0300

    addd: finalização do codigo de animais com polimorfismo

--- POO-Animais/src/Main.java
+++ POO-Animais/src/Main.java
@@ -1,26 +1,149 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 class Main {
+    static Scanner sc = new Scanner(System.in);
+    static ArrayList<Animal> animals = new ArrayList<Animal>();
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        showAnimalsMenu();
+    }
 
-        Animal[] animals = new Animal[3];
+    static void showAnimalsMenu() {
+        System.out.println("=== Create new Animals Menu ===");
+        System.out.println("1. Register new animal;");
+        System.out.println("2. Show all animals;");
+        System.out.println("3. Move animals;");
+        System.out.println("4. Edit animal;");
+        System.out.println("5. Exit;");
 
-        //Animais em geral
-        animals[0] = new Animal("Gato", 4, 1, 0);
-        System.out.println(animals[0].toString());
+        System.out.println("Select: ");
+        int userChoice = sc.nextInt();
 
-        //Peixe
-        animals[1] = new Fish("Bolota", 0, 3, 0, (byte) 1);
-        System.out.println(animals[1].toString());
+        choiceAnimalMenu(userChoice);
+    }
 
-        //Aves
-        animals[2] = new Bird("Piupiu Pistola", 2, 15, 0);
-        System.out.println(animals[0].toString());
-
-        for (Animal a : animals) {
-            a.move();
-            System.out.println("Localization: " + a.getLocalization());
+    static void choiceAnimalMenu(int userChoice) {
+        switch (userChoice) {
+            case 1:
+                System.out.println("=== Register new animal ===");
+                showMenuRegisterNewAnimal();
+                showAnimalsMenu();
+                break;
+            case 2:
+                System.out.println("=== Show all animals ===");
+                showAllAnimals();
+                showAnimalsMenu();
+                break;
+            case 3:
+                System.out.println("=== Move animal ===");
+                showMoveAnimalMenu();
+                showAnimalsMenu();
+                break;
+            case 4:
+                System.out.println("=== Edit animal ===");
+                showEditAnimalMenu();
+                showAnimalsMenu();
+                break;
+            case 5:
+                System.out.println("Exiting....");
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Invalid choice");
+                showAnimalsMenu();
+                break;
         }
+    }
+
+    static void showMenuRegisterNewAnimal() {
+        System.out.println("=== Select the Species === ");
+        System.out.println("1. Animal (Geral);");
+        System.out.println("2. Fish;");
+        System.out.println("3. Bird;");
+
+        int userChoice = sc.nextInt();
+        choiceMenuRegisterNewAnimal(userChoice);
+    }
+
+    static void choiceMenuRegisterNewAnimal(int userChoice) {
+        System.out.println("Write the animal's name: ");
+        String name = sc.next();
+
+        System.out.println("Write the animal's paws:");
+        int paws = sc.nextInt();
+
+        System.out.println("Write the animal's speed:");
+        double speed = sc.nextDouble();
+
+        System.out.println("Write the animal's localization:");
+        int localization = sc.nextInt();
+
+        switch (userChoice) {
+            case 1:
+                animals.add(new Animal(name, paws, speed, localization));
+                System.out.println("Create new animal successfully!");
+                break;
+            case 2:
+                System.out.println("Write the fish's water type (0 > Freshwater; 1 > Sea Water)  : ");
+                byte waterType = sc.nextByte();
+                animals.add(new Fish(name, paws, speed, localization, waterType));
+                System.out.println("Create new fish successfully!");
+                break;
+            case 3:
+                animals.add(new Bird(name, paws, speed, localization));
+                System.out.println("Create new bird successfully!");
+                break;
+        }
+    }
+
+    static void showAllAnimals() {
+        if (animals.isEmpty()) {
+            System.out.println("There are no animals!!");
+            showAnimalsMenu();
+        } else {
+            for (int i = 0; i < animals.size(); i++) {
+                System.out.println((i + 1) + ". " + animals.get(i).toString());
+            }
+        }
+    }
+
+    static void showMoveAnimalMenu() {
+        showAllAnimals();
+
+        System.out.println("Select: ");
+        int userChoice = sc.nextInt();
+
+        animals.get(userChoice - 1).move();
+    }
+
+    static void showEditAnimalMenu() {
+        showAllAnimals();
+        System.out.println("Select: ");
+        int userChoice = sc.nextInt();
+
+        System.out.println("Write the animal's name: ");
+        String name = sc.next();
+        animals.get(userChoice - 1).setName(name);
+
+        System.out.println("Write the animal's paws:");
+        int paws = sc.nextInt();
+        animals.get(userChoice - 1).setPaws(paws);
+
+        System.out.println("Write the animal's speed:");
+        double speed = sc.nextDouble();
+        animals.get(userChoice - 1).setSpeed(speed);
+
+        System.out.println("Write the animal's localization:");
+        int localization = sc.nextInt();
+        animals.get(userChoice - 1).setLocalization(localization);
+
+        if (animals.get(userChoice - 1) instanceof Fish) {
+            System.out.println("Write the fish's water type (0 > Freshwater; 1 > Sea Water): ");
+            byte waterType = sc.nextByte();
+            ((Fish) animals.get(userChoice - 1)).setWaterType(waterType);
+        }
+
+        System.out.println("Edit animal successfully!");
     }
 }
