bb78435231f02edc29ad36ffb3c64c85520bc198
Pedro da Costa Lopes <pedrolopes.programador013@gmail.com>
2026-06-01 19:59:04 -0300

    addd: criacao do POST no PessoaDAO

--- ConexaoDB/src/main/java/com/example/model/dao/PessoaDAO.java
+++ ConexaoDB/src/main/java/com/example/model/dao/PessoaDAO.java
@@ -1,6 +1,8 @@
 package com.example.model.dao;
 import com.example.model.Pessoa;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class PessoaDAO {
     private Connection con;
@@ -11,6 +13,19 @@ public class PessoaDAO {
 
     //INSERT
     public void inserir(Pessoa pessoa){
-        String sql = "INSERT INTO pessoa VALUES (nome, email)";
+        String sql = "INSERT INTO pessoa(nome, email) VALUES (?, ?)";
+        try{
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, pessoa.getNome());
+                                     //^-- aqui fica a posição da interrogação (?)
+            stmt.setString(2, pessoa.getEmail());
+                                            //^-- aqui fica o valor que será inserido
+            stmt.execute();//<-- onde executou a QUERY
+
+            System.out.println("Pessoa: " + pessoa.getNome() + " salvo com sucesso!");
+
+        }catch(SQLException e){
+
+        }
     }
 }
